32845172bb316c7942dd4c039e8d462a49bb323d
JFSilvaBr <jfsilvabr@gmail.com>
2021-05-25 01:30:48 -0300

    add folder cmsimple/plugins for project

---
+++ cmsimple/plugins/fa/languages/pt.php
@@ -0,0 +1,14 @@
+<?php
+
+$plugin_tx['fa']['alt_logo']="Fort Awesome";
+$plugin_tx['fa']['syscheck_fail']="failure";
+$plugin_tx['fa']['syscheck_message']="Checking that %1\$s … %2\$s";
+$plugin_tx['fa']['syscheck_phpversion']="Versão do PHP ≥ %s";
+$plugin_tx['fa']['syscheck_success']="okay";
+$plugin_tx['fa']['syscheck_title']="System check";
+$plugin_tx['fa']['syscheck_warning']="warning";
+$plugin_tx['fa']['syscheck_writable']="'%s' is writable";
+$plugin_tx['fa']['syscheck_xhversion']="Versão do CMSimple_XH ≥ %s";
+$plugin_tx['fa']['cf_require_auto']="Whether Font Awesome is always available.";
+
+?>
